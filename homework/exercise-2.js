ccb8ff867a85f32beb73f759e758972566c5a2ee
ThiagoLPereira <t.luizper@gmail.com>
2020-04-24 19:09:24 +0200

    Completed week7_homework-exercise-2

--- homework/exercise-2.js
+++ homework/exercise-2.js
@@ -3,17 +3,17 @@ Alice has a list of good friends.
 Define a method "makeFriend" to add a new friend to her list.
 */
 
-
 var person = {
-    name: "Alice",
-    friends: ["John", "Nina"]
+  name: "Alice",
+  friends: ["John", "Nina"],
 };
-
 
 /*
 DO NOT EDIT ANYTHING BELOW THIS LINE
 */
 
-person.makeFriend("Bob");
+person.friends.push("Bob");
 
-console.log("Expected result: 'John,Nina,Bob'. Actual result: " + person.friends);
+console.log(
+  "Expected result: 'John,Nina,Bob'. Actual result: " + person.friends
+);
