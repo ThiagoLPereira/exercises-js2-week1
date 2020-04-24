9bc6469db4b0c98e9972d7c589f73e39c7039f94
ThiagoLPereira <t.luizper@gmail.com>
2020-04-24 17:53:27 +0200

    Completed week_7_Objects_intro-exercise-0

--- class/A-objects-intro/exercise-part-0.js
+++ class/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,14 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+var laptop = {
+  brand: "HP",
+  model: "ProBook",
+  color: "Silver",
+  screenSize: 13,
+  isTouchscreen: false,
+  memory: 16,
+  MAXBatery: 10,
+};
+
+console.log(laptop);
