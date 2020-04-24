12ae3b7ce8a5508723b979d09234b50d3d9e4436
ThiagoLPereira <t.luizper@gmail.com>
2020-04-24 17:54:41 +0200

    Completed week_7_Objects_intro-exercise-1

--- class/A-objects-intro/exercise-part-1.js
+++ class/A-objects-intro/exercise-part-1.js
@@ -17,3 +17,48 @@ var car = {
 
 */
 
+var smartphone = {
+  brand: "Google",
+  model: "Google Pixel 4 XL",
+  processor: "Qualcomm Snapdragon 855, Pixel Neural Core.",
+  memory: "128GB",
+  cameras: "12MP Dual-Pixel and 16MP Telephoto",
+};
+console.log(smartphone);
+
+var houseToRent = {
+  bathroom: 2,
+  rooms: 3,
+  garden: true,
+  centalVentilation: true,
+  garage: false,
+};
+console.log(houseToRent);
+
+var speaker = {
+  frequency: "120hz-20KHz",
+  internalMagneticHorn: "4Ω5W x 2",
+  BatteryCapacity: "1200mAh",
+  bluetooth: true,
+  WorkingVoltage: "DC 5V, 3.7",
+};
+console.log(speaker);
+
+var smartlight = {
+  packagedQuantity: 1,
+  bulbs: true,
+  wifiConection: true,
+  iOScompatible: "Android, WindowsPhone, iOS",
+  wattage: "27 W",
+  manufacturer: "Philips",
+};
+console.log(smartlight);
+
+var eBike = {
+  weight: "14.5kg",
+  material: "Aluminum",
+  batteryCapacity: "5800mAh-208.8Wh",
+  chargingTime: "3hours",
+  motorSpeed: "20km/H",
+};
+console.log(eBike);
