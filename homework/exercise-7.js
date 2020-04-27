3a3528e1d44416789469b89ec85552f426373116
ThiagoLPereira <t.luizper@gmail.com>
2020-04-27 02:44:37 +0200

    Completed week7_homework-exercise-7

--- homework/exercise-7.js
+++ homework/exercise-7.js
@@ -6,7 +6,7 @@ and for each object, `console.log()` out the sentence:
 
 Here is the array:
 
-*/ 
+*/
 
 var writers = [
   {
@@ -14,31 +14,50 @@ var writers = [
     lastName: "Woolf",
     occupation: "writer",
     age: 59,
-    alive: false
+    alive: false,
   },
   {
     firstName: "Zadie",
     lastName: "Smith",
     occupation: "writer",
     age: 41,
-    alive: true
+    alive: true,
   },
   {
     firstName: "Jane",
     lastName: "Austen",
     occupation: "writer",
     age: 41,
-    alive: false
+    alive: false,
   },
   {
     firstName: "bell",
     lastName: "hooks",
     occupation: "writer",
     age: 64,
-    alive: true
+    alive: true,
   },
 ];
 
+for (var i = 0; i < writers.length; i++) {
+  var write = writers[i];
+  var sentence = getWriterSentence(writer);
+  console.log(sentence);
+}
+
+function getWriterSentence(writer) {
+  var sentence = "Hi, my name is" + writer.firstName + "";
+  sentence += writer.lastName + " I am " + writer.age;
+  sentence += "years old, and work as a" + write.occupation + ".";
+  return sentence;
+}
 /*
 If you want an extra challenge, only `console.log()` the writers that are alive.
 */
+
+write.forEach(function (write) {
+  if (write.alive) {
+    var sentence = getWriterSentence(write);
+    console.log(sentence);
+  }
+});
